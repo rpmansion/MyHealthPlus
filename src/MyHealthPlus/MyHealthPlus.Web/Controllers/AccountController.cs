029cb97451f80cb2b171fa7b6957b84b3612ac8a
Robert Mansion <robertp.mansion@gmail.com>
2020-03-18 01:54:43 +0800

    fix issue with registration

--- src/MyHealthPlus/MyHealthPlus.Web/Controllers/AccountController.cs
+++ src/MyHealthPlus/MyHealthPlus.Web/Controllers/AccountController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyHealthPlus.Data.Models;
 using MyHealthPlus.Web.Models;
 using System.Threading.Tasks;
+using IdentityModel;
 
 namespace MyHealthPlus.Web.Controllers
 {
@@ -11,20 +15,36 @@ namespace MyHealthPlus.Web.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
+        private readonly SignInManager<Account> _signInManager;
         private readonly UserManager<Account> _userManager;
         private readonly ILogger<AccountController> _logger;
 
         public AccountController(
             UserManager<Account> userManager,
+            SignInManager<Account> signInManager,
             ILogger<AccountController> logger)
         {
             _logger = logger;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpPost("login")]
-        public IActionResult Login([FromBody]LoginModel model)
+        public async Task<IActionResult> LoginAsync([FromBody]LoginModel model)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+
+            //if (!result.Succeeded)
+            //{
+            //    // TODO : Log error
+            //    return BadRequest(result.Errors.First().Description);
+            //}
+
             return Ok();
         }
 
@@ -36,10 +56,44 @@ namespace MyHealthPlus.Web.Controllers
                 return BadRequest(ModelState);
             }
 
-            // var result = await _userManager.CreateAsync(userIdentity, model.Password);
-            // var result = await _userManager.CreateAsync(new Account(), model)
+            var account = new Account
+            {
+                UserName = model.Email
+            };
 
-            return Ok();
+            var result = await _userManager.CreateAsync(account, model.Password);
+
+            if (!result.Succeeded)
+            {
+                // TODO : Log error
+                return BadRequest(result.Errors.First().Description);
+            }
+
+            result = await _userManager.AddClaimsAsync(account, new Claim[]{
+                new Claim(JwtClaimTypes.Name, $"{model.FirstName} {model.LastName}"),
+                new Claim(JwtClaimTypes.MiddleName, model.MiddleName),
+                new Claim(JwtClaimTypes.GivenName, model.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, model.LastName),
+                new Claim(JwtClaimTypes.Email, model.Email),
+                new Claim(JwtClaimTypes.Role, "Patient"),
+                new Claim(JwtClaimTypes.PhoneNumber, "")
+            });
+
+            if (!result.Succeeded)
+            {
+                // TODO : Log error
+                return BadRequest(result.Errors.First().Description);
+            }
+
+            result = await _userManager.AddToRoleAsync(account, "Patient");
+
+            if (!result.Succeeded)
+            {
+                // TODO : Log error
+                return BadRequest(result.Errors.First().Description);
+            }
+
+            return Ok("Successfully created an account for you.");
         }
     }
 }
